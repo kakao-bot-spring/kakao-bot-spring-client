0bf50a34b869b3c8bea19441c664ae60dab56ec2
thuthi <whqdudn55@gmail.com>
2023-03-02 17:29:53 +0900

    refactor: connection 설정 코드 리팩토링

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const server_connection_config = {
     port: 8080 // Spring 서버 Port
 };
 
+let establishConnection = function() {
+    try {
+        let connection = (new java.net.URL(server_connection_config.address + ":" + server_connection_config.port)).openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+        return connection;
+    } catch (e) {
+        Log.e(e);
+    }
+
+    return null;
+}
+
 let writeConnectionData = function(connection, jsonString) {
     try {
         let writeStream = new java.io.DataOutputStream(connection.getOutputStream());
@@ -30,15 +44,9 @@ let httpPost = function(connection) {
 }
 
 let response = function(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-    try {
-        let connection = (new java.net.URL(server_connection_config.address + ":" + server_connection_config.port)).openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "application/json");
-        writeConnectionData(connection, msg);
-        let res = httpPost(connection);
-        Log.d(res);
-    } catch (e) {
-        Log.e(e);
+    const connection = establishConnection();
+    if (connection == null) {
+        Log.e("server connection failed");
+        return;
     }
 }
